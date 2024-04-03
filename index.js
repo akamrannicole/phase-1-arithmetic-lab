5032b18f81bdd9131e981f55721c566a4daea05a
user.name <akamrannicole@gmail.com>
2024-04-03 10:20:42 +0300

    arithmetic lab done

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply);//output : 62
+
+let random = Math.ceil (Math.random() *100);
+console.log(random);
+
+let num3 = 22;
+let num4 = 9;
+let mod = num3 % num4;
+console.log(mod);//output : 4 
+
+let max = Math.max(1, 3, 5, 6, 13, 16,20,9);
+console.log(max);
+
+
